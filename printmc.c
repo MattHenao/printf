a33373e588bcc74060a9b3ff5cc82d30a29d9a24
MattHenao <2340@holbertonschool.com>
2021-03-16 19:27:23 +0000

    My first function about printf

---
+++ printmc.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <unistd.h>
+
+/**
+ * _printf - My function about printf
+ *
+ * @format: Text that i have to print
+ *
+ * Return: 0?
+ */
+
+int _printf(const char *format, ...);
+{
+	int i;
+
+	for (i = 0; format[i] != '\0'; i++)
+	;
+	write(1, c, i);
+	return (0);
+}
