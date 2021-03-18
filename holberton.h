3770491f6b9e7f9a8c31a1bbc73a49bc53df7b23
MattHenao <2340@holbertonschool.com>
2021-03-18 03:21:58 +0000

    Libraries

--- holberton.h
+++ holberton.h
@@ -3,7 +3,12 @@
 
 #include <stdlib.h>
 #include <unistd.h>
+#include <stdarg.h>
+#include <stdio.h>
 
 int _printf(const char *format, ...);
+int main(void);
+void func_s (va_list li);
+void func_c (va_list li);
 
 #endif
