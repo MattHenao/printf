dec8ea3c0f3ff39833b036d62a445f19f35699ea
MattHenao <2340@holbertonschool.com>
2021-03-19 15:42:39 +0000

    Function String

--- func_s.c
+++ func_s.c
@@ -4,17 +4,21 @@
  * func_s - Detecte if a data type is a string
  *
  * @li: List
+ *
+ * Return: Count
  */
 
-void func_s(va_list li)
+int func_s(va_list li)
 {
 	char *p;
-	int a;
+	int a, count = 0;
 
 	p = va_arg(li, char *);
 
 	for (a = 0; p[a] != '\0'; a++)
 	{
 		write(1, &p[a], 1);
+		count++;
 	}
+	return (count);
 }
